d5b118d544b2388b06bf3a88a716a940d33b2c88
deboker <117391017+deboker@users.noreply.github.com>
2025-11-01 23:30:57 +0100

    scrool up

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
 
 function Terms() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="app">
       <div className="legal-page">
